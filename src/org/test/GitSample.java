9fd30fda3b8bc52fac73824f7943e43e1db9b6bc
SindhuJudison <samsv0511@gmail.com>
2022-03-02 15:18:39 +0530

    Git

---
+++ src/org/test/GitSample.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class GitSample {
+	
+	public void gitone() {
+    System.out.println("Test one"); 
+	}
+
+	public void gittwo() {
+    System.out.println("Test two"); 
+	}
+	
+	public static void main(String[] args) {
+		GitSample sample =  new GitSample();
+		sample.gitone();
+		sample.gittwo();
+	}
+}
